5647d402bff34a35c675e46b5a36ac09cefaa7ec
gaelitoelquesito <gaelitoelquesito@gmail.com>
2017-06-26 00:59:47 +0200

    Tests

--- src/main/java/es/minetsii/wetspongetest/WetSpongeTest.java
+++ src/main/java/es/minetsii/wetspongetest/WetSpongeTest.java
@@ -1,33 +1,31 @@
 package es.minetsii.wetspongetest;
 
 
-import es.minetsii.wetsponge.WetSponge;
-import es.minetsii.wetsponge.entity.living.animal.WSPig;
-import es.minetsii.wetsponge.plugin.WSPlugin;
+import com.degoos.wetsponge.WetSponge;
+import com.degoos.wetsponge.enums.EnumSoundCategory;
+import com.degoos.wetsponge.enums.EnumTextColor;
+import com.degoos.wetsponge.event.WSListener;
+import com.degoos.wetsponge.event.entity.player.connection.WSPlayerJoinEvent;
+import com.degoos.wetsponge.plugin.WSPlugin;
+import com.degoos.wetsponge.sound.WSSound;
+import com.degoos.wetsponge.text.WSText;
 
 public class WetSpongeTest extends WSPlugin {
 
-	@Override
-	public void onEnable () {
-		System.out.println("Hi! I'm a plugin!");
-		WetSponge.getEventManager().registerListener(this, this);
-		new Thread(() -> {
-			while (WetSpongeTest.this.isEnabled()) {
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				WetSponge.getServer().getWorlds().forEach(world -> world.getEntities().stream().filter(wsEntity -> wsEntity instanceof WSPig).forEach(pig -> {
-					((WSPig)pig).setSaddled(true);
-				}));
-			}
-		}).start();
-	}
+    @Override
+    public void onEnable() {
+        WetSponge.getEventManager().registerListener(this, this);
+    }
 
 
-	@Override
-	public void onDisable () {
-		System.out.println("Goodbye!");
-	}
+    @Override
+    public void onDisable() {
+        WetSponge.getServer().getConsole().sendMessage(WSText.builder("Goodbye!").color(EnumTextColor.RED).build());
+    }
+
+    @WSListener
+    public void onPlayerJoin(WSPlayerJoinEvent event) {
+        event.getPlayer().playSound(WSSound.ENTITY_PLAYER_LEVELUP, 1, 1);
+    }
+
 }
